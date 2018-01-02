fd882aaec4143b0e16c9859b9c1a14d909eda2e7
Infosys App Development team <Infy.devops.poc2@outlook.com>
2018-01-02 11:43:37 +0530

    test

--- dotnetcoreapp/dotnetcoreapptest/UnitTest1.cs
+++ dotnetcoreapp/dotnetcoreapptest/UnitTest1.cs
@@ -7,8 +7,10 @@ namespace dotnetcoreapptest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public bool IsOneOne()
         {
+            if (1 == 1)
+                return true;
         }
     }
 }
